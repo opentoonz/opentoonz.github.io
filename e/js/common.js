6dc81dbd6b7416cd568204685f46014279f95203
Masayuki Kimura <masayuki_kimura@dwango.co.jp>
2018-08-23 22:20:58 +0900

    タイトルのCSSとjsを修正

--- e/js/common.js
+++ e/js/common.js
@@ -6,6 +6,18 @@ $(function(){
 		return false;
 	});
 
+	$(".sitewide_SpMenuList_Container a").on('click touchend', function(){
+		$('#nav-input').prop('checked', false);
+
+		$('a[href^=#]').click(function(){
+			var speed = 500;
+			var href= $(this).attr("href");
+			var target = $(href == "#" || href == "" ? 'html' : href);
+			var position = target.offset().top; $("html, body").animate({scrollTop:position}, speed, "swing");
+			return false;
+		});
+	});
+
 	$('a[href^=#]').click(function(){
 		var speed = 500;
 		var href= $(this).attr("href");
