54ddde29095c1e5ff291781b977e76fcc0761793
Pikhosh <73311467+pikhosh@users.noreply.github.com>
2021-05-29 22:18:38 +0430

    Fix Header problem when opening website

--- e/js/header.js
+++ e/js/header.js
@@ -3,11 +3,11 @@ $(function() {
         if ($(window).scrollTop() > 50) {
             $("header").addClass("active");
             $(".sitewide_HeaderMenu_Container").toggleClass("active_sitewide_HeaderMenu_Container", true);
-            $(".sitewide_HeaderMenu_PcLogo img").addClass("active_sitewide_HeaderMenu_PcLogo");
+            $(".sitewide_HeaderMenu_PcLogo img").toggleClass("active_sitewide_HeaderMenu_PcLogo", true);
         } else {
             $("header").removeClass("active");
             $(".sitewide_HeaderMenu_Container").removeClass("active_sitewide_HeaderMenu_Container");
-            $(".sitewide_HeaderMenu_PcLogo img").toggleClass("active_sitewide_HeaderMenu_PcLogo", false);
+            $(".sitewide_HeaderMenu_PcLogo img").removeClass("active_sitewide_HeaderMenu_PcLogo");
         }
     });
 });
