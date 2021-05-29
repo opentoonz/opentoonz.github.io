df3bcdcd21146b3ea1b5a97d5baee3cfcb227e8b
Pikhosh <73311467+pikhosh@users.noreply.github.com>
2021-05-29 22:03:02 +0430

    Fix Header problem when opening website

--- e/js/header.js
+++ e/js/header.js
@@ -2,14 +2,12 @@ $(function() {
     $(window).on("scroll", function() {
         if ($(window).scrollTop() > 50) {
             $("header").addClass("active");
-            $(".sitewide_HeaderMenu_Container").addClass("active_sitewide_HeaderMenu_Container");
-            $(".sitewide_HeaderMenu_Container").removeClass("deactive_sitewide_HeaderMenu_Container");
+            $(".sitewide_HeaderMenu_Container").toggleClass("active_sitewide_HeaderMenu_Container", true);
             $(".sitewide_HeaderMenu_PcLogo img").addClass("active_sitewide_HeaderMenu_PcLogo");
             $(".sitewide_HeaderMenu_PcLogo img").removeClass("dective_sitewide_HeaderMenu_PcLogo");
         } else {
             $("header").removeClass("active");
             $(".sitewide_HeaderMenu_Container").removeClass("active_sitewide_HeaderMenu_Container");
-            $(".sitewide_HeaderMenu_Container").addClass("deactive_sitewide_HeaderMenu_Container");
             $(".sitewide_HeaderMenu_PcLogo img").removeClass("active_sitewide_HeaderMenu_PcLogo");
             $(".sitewide_HeaderMenu_PcLogo img").addClass("dective_sitewide_HeaderMenu_PcLogo");
         }
