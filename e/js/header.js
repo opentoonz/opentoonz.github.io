0c18ab3b7fdace5c5441c974ef45a16352d74555
Pikhosh <73311467+pikhosh@users.noreply.github.com>
2021-05-31 20:50:22 +0430

    Fix mobile version problems and apply new changes
    It remains to add new links to the header menu...
    And I noticed a problem with the new changes for the mobile version; I will fix it as soon as I find a solution!

--- e/js/header.js
+++ e/js/header.js
@@ -4,10 +4,14 @@ $(function() {
             $("header").addClass("active_header");
             $(".sitewide_HeaderMenu_Container").toggleClass("active_sitewide_HeaderMenu_Container", true);
             $(".sitewide_HeaderMenu_PcLogo img").toggleClass("active_sitewide_HeaderMenu_PcLogo", true);
+            $(".sitewide_HeaderMenu_SpLogo").toggleClass("acitve_sitewide_HeaderMenu_SpLogo", true);
+            $(".sitewide_HamburgerMenu_Container").toggleClass("active_sitewide_HamburgerMenu_Container", true);
         } else {
             $("header").removeClass("active_header");
             $(".sitewide_HeaderMenu_Container").removeClass("active_sitewide_HeaderMenu_Container");
             $(".sitewide_HeaderMenu_PcLogo img").removeClass("active_sitewide_HeaderMenu_PcLogo");
+            $(".sitewide_HeaderMenu_SpLogo").removeClass("acitve_sitewide_HeaderMenu_SpLogo");
+            $(".sitewide_HamburgerMenu_Container").removeClass("active_sitewide_HamburgerMenu_Container");
         }
     });
 
